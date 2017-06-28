9db871af4d4698bb8a3295504239719eee1b6043
Kumar Kartikay <krkartikay@gmail.com>
2017-06-28 14:42:51 +0530

    Check each word individually for respect words

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 exports.terms = {
-    RESPECT : ['sir', 'Sir', 'maam', 'Maam', 'mam', 'Mam', 'Ma\'am']
+    RESPECT : ['sir', 'maam', 'mam', 'ma\'am']
 }
 
 exports.weather = {
--- observers/respect-terms.js
+++ observers/respect-terms.js
@@ -7,14 +7,18 @@ var utils = require('../utils');
 var logging = require('../config/logger');    
 
 module.exports = function(bot, from, to, message) {
-	var noRespectTerms = constants.terms.RESPECT;
+    var noRespectTerms = constants.terms.RESPECT;
     for (var i in noRespectTerms) {
         var substring = noRespectTerms[i];
-        if (~message.indexOf(substring)) {
-            logging.info("Respect Terms used by " + from);
-            return ("Hey " + from + " please don't use " + c.pink(noRespectTerms[i]) + " to address people. Use nicks instead.\n" 
-            	    + c.underline.green('Modified:') + ' ' + message.replace(noRespectTerms[i], ""));
+        var words = message.split(" ");
+        for (var j in words){
+            var currentword = words[j];
+            currentword = currentword.toLowerCase();
+            if (currentword.startsWith(substring)) {
+                logging.info("Respect Terms used by " + from);
+                return ("Hey " + from + " please don't use " + c.pink(noRespectTerms[i]) + " to address people. Use nicks instead.\n" 
+                      + c.underline.green('Modified:') + ' ' + message.replace(noRespectTerms[i], ""));
+            }
         }
     }
 }
-    
